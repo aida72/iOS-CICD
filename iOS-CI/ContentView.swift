b8b04b345472f56cc825963b823bdb9d9dce1d37
Bhaumik Chaudhari <bhaumik.31.10@gmail.com>
2019-12-15 11:10:19 -0500

    Initial Commit

---
+++ iOS-CI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS-CI
+//
+//  Created by Bhaumik on 2019-12-15.
+//  Copyright © 2019 Bhaumik. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
